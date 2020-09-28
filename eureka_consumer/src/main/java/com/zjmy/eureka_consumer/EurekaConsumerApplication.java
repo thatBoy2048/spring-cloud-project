d85c2836b41d8bb5f983b47351ef5127a821255b
zhongxiang <zhongxiang@moors.com.cn>
2020-09-28 17:44:01 +0800

    Ribbon-负载均衡

--- eureka_consumer/src/main/java/com/zjmy/eureka_consumer/EurekaConsumerApplication.java
+++ eureka_consumer/src/main/java/com/zjmy/eureka_consumer/EurekaConsumerApplication.java
@@ -1,12 +1,14 @@
 package com.zjmy.eureka_consumer;
 
+import com.zjmy.myrule.MySelRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+//@RibbonClient(name = "EUREKA-PROVIDER-ATTA",configuration = MySelRule.class)
 public class EurekaConsumerApplication {
 
     public static void main(String[] args) {
