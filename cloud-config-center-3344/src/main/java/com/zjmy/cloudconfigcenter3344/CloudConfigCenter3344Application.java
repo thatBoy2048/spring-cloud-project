838dd61f060d3668dd90d1b38421cba94496bfcc
zhongxiang <zhongxiang@moors.com.cn>
2020-10-27 17:12:26 +0800

    config

---
+++ cloud-config-center-3344/src/main/java/com/zjmy/cloudconfigcenter3344/CloudConfigCenter3344Application.java
@@ -0,0 +1,13 @@
+package com.zjmy.cloudconfigcenter3344;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudConfigCenter3344Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudConfigCenter3344Application.class, args);
+    }
+
+}
