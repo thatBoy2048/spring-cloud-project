16dc2947ae3f5296173d4b3480b6b131cbca3ff6
zhongxiang <zhongxiang@moors.com.cn>
2020-11-02 17:43:03 +0800

    'sleuth'

---
+++ cloud-stream-rabbitmq-comsumer-8803/src/main/java/com/zjmy/cloudstreamrabbitmqcomsumer8803/CloudStreamRabbitmqComsumer8803Application.java
@@ -0,0 +1,13 @@
+package com.zjmy.cloudstreamrabbitmqcomsumer8803;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudStreamRabbitmqComsumer8803Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudStreamRabbitmqComsumer8803Application.class, args);
+    }
+
+}
---
+++ cloud-stream-rabbitmq-comsumer-8803/src/test/java/com/zjmy/cloudstreamrabbitmqcomsumer8803/CloudStreamRabbitmqComsumer8803ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zjmy.cloudstreamrabbitmqcomsumer8803;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudStreamRabbitmqComsumer8803ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
