a0a63193940eb5afbe68fc4a3529df4f7aad9291
zhongxiang <zhongxiang@moors.com.cn>
2021-03-16 15:37:25 +0800

    security-new

--- security/src/main/java/com/zjmy/security/config/WebConfig.java
+++ security/src/main/java/com/zjmy/security/config/WebConfig.java
@@ -11,7 +11,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-
+//
+//    @Override
+//    public void addViewControllers(ViewControllerRegistry registry) {
+//        registry.addViewController("/").setViewName("redirect:/login-view");
+//        registry.addViewController("/login-view").setViewName("login");
+//
+//    }
+//
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("redirect:/login-view");
